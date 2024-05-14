41101352a957e792f670fbbf6eb8bb2cae470963
Aidan Middel <aidanm@ctjies.nl>
2024-05-14 10:37:56 +0200

    add page and 4th test slide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import "./globals.css";
 import Navigation from "../components/navigationbar/navigationbar";
 import React from 'react';
+import Carousel from "@/components/carousel/Carousel";
+import slides from '../components/carousel/carouselData.json'
 
 const Page: React.FC = () => {
   return (
     <header>
       <Navigation /> {/* Rendering the imported content component */}
+      <Carousel data={slides}/>
     </header>
   );
 };

37477a1c351a8417f4550eab3030f1b319fffc2b
Amyvd <30554@ma-web.nl>
2024-05-27 22:19:54 +0200

    Partners pages added with images and links to partners pages

--- src/data/pageData.tsx
+++ src/data/pageData.tsx
@@ -13,6 +13,23 @@ import SearchComponent from "../components/searchFunction/searchFunction"
 import "../app/globals.css";
 import "../styles/navbar.css";
 
+
+/*  IMPORT IMAGES   */
+import StedelijkMuseumImage from "../../public/images/partners/stedelijke .png"
+import WaagFutureLabImage from "../../public/images/partners/waag futurelab.png"
+import ruudVanOosterhoutDesignImage from "../../public/images/partners/ruud van oosterhout design.png"
+import RobertBronwasser from "../../public/images/partners/robert bronwasser.png"
+import bnoImage from "../../public/images/partners/bno made by dutch designwers.png"
+import interieurClubImage from "../../public/images/partners/de interieur club.png"
+import roetz from "../../public/images/partners/roetz.png"
+import CreativeIndustriesFundNL from "../../public/images/partners/craetive industies fund nl.png"
+import afkImage from "../../public/images/partners/afk.png"
+import salto from "../../public/images/partners/salto.png"
+import basdegroot from "../../public/images/partners/bas de groot.png"
+import mediacollege from "../../public/images/partners/mediacollege.png"
+import PakhuisDeZwijger from "../../public/images/partners/pakhuis de zwijger.png"
+
+
 export const PageData: PageDataProps[] = [
   {
     path: "/",
@@ -21,7 +38,7 @@ export const PageData: PageDataProps[] = [
     navClass: "",
     content: (
       <div >
-      
+
       </div>
     )
   },
@@ -78,7 +95,7 @@ export const PageData: PageDataProps[] = [
     component: "MappPage",
     navClass: "map__link",
   },
-  
+
 ]
 
 export const extraPaths: SidePageDataProps[] = [
@@ -180,8 +197,8 @@ export const extraPaths: SidePageDataProps[] = [
         <SidebarNavAboutComponent />
         <p>
           GLUE amsterdam connected by design is made possible by the support of our Founding and Cultural Partners. Their businesses are strongly tied with the culture and economy of Amsterdam. Who are they?
-        </p><br/>
-          <SidebarNavPartnersComponent />
+        </p><br />
+        <SidebarNavPartnersComponent />
       </div>
     )
   },
@@ -315,7 +332,14 @@ export const extraPaths: SidePageDataProps[] = [
     component: "culturalpartnerspage",
     content: (
       <div className="pageLayout">
-
+        <h1 className="pageLayout__title">Cultural Partners</h1>
+        <SidebarNavAboutComponent />
+        <div className="pageLayout__pageImage">
+        <Link href="https://www.stedelijk.nl/nl/">
+          <Image src={StedelijkMuseumImage} alt="" className="pagefundingImage" style={{ width: '35rem', height: 'auto' }} />
+          </Link>
+        <Link href="https://waag.org/nl/"><Image src={WaagFutureLabImage} alt="" className="pagefundingImage" style={{ width: '35rem' }} /></Link>
+        </div>
       </div>
     )
   },
@@ -325,7 +349,12 @@ export const extraPaths: SidePageDataProps[] = [
     component: "Foundingpartnerspage",
     content: (
       <div className="pageLayout">
-
+        <h1 className="pageLayout__title">Founding Partners</h1>
+        <SidebarNavAboutComponent />
+        <div className="pageLayout__pageImage">
+          <Link href="https://www.rvodesign.com/projects/"><Image src={ruudVanOosterhoutDesignImage} alt="" className="pagefundingImage" style={{ width: '25rem', height: 'auto' }} /></Link>
+          <Link href="https://www.robertbronwasser.com/"><Image src={RobertBronwasser} alt="" className="pagefundingImage" style={{ width: '25rem' }} /></Link>
+        </div>
       </div>
     )
   },
@@ -335,7 +364,12 @@ export const extraPaths: SidePageDataProps[] = [
     component: "communitypartnerspage",
     content: (
       <div className="pageLayout">
-
+        <h1 className="pageLayout__title">Community Partners</h1>
+        <SidebarNavAboutComponent />
+        <div className="pageLayout__pageImage">
+          <Link href="https://www.bno.nl/"><Image src={bnoImage} alt="" className="pagefundingImage" style={{ width: '25rem', height: 'auto' }} /></Link>
+          <Link href="https://www.deinterieurclub.com/"><Image src={interieurClubImage} alt="" className="pagefundingImage" style={{ width: '25rem' }} /></Link>
+        </div>
       </div>
     )
   },
@@ -345,7 +379,11 @@ export const extraPaths: SidePageDataProps[] = [
     component: "mobilitypartnerspage",
     content: (
       <div className="pageLayout">
-
+        <h1 className="pageLayout__title">Mobility Partners</h1>
+        <SidebarNavAboutComponent />
+        <div className="pageLayout__pageImage">
+          <Link href="https://roetz-bikes.com/nl_NL/"><Image src={roetz} alt="" className="pagefundingImage" style={{ width: '25rem', height: 'auto' }} /></Link>
+        </div>
       </div>
     )
   },
@@ -355,7 +393,12 @@ export const extraPaths: SidePageDataProps[] = [
     component: "fundingpartnerspage",
     content: (
       <div className="pageLayout">
-
+        <h1 className="pageLayout__title">Funding Partners</h1>
+        <SidebarNavAboutComponent />
+        <div className="pageLayout__pageImage">
+          <Link href="https://www.stimuleringsfonds.nl/"><Image src={CreativeIndustriesFundNL} alt="" className="pagefundingImage" style={{ width: '25rem', height: 'auto' }} /></Link>
+          <Link href="https://www.amsterdamsfondsvoordekunst.nl/"><Image src={afkImage} alt="" className="pagefundingImage" style={{ width: '25rem' }} /></Link>
+        </div>
       </div>
     )
   },
@@ -365,7 +408,14 @@ export const extraPaths: SidePageDataProps[] = [
     component: "broadcastingpartnerspage",
     content: (
       <div className="pageLayout">
-
+        <h1 className="pageLayout__title">Broadcasting Partners</h1>
+        <SidebarNavAboutComponent />
+        <div className="pageLayout__pageImage">
+          <Link href="https://www.salto.nl/"><Image src={salto} alt="" className="pagefundingImage" style={{ width: '25rem', height: 'auto' }} /></Link>
+          <Link href="https://basmaaktfilms.com/"><Image src={basdegroot} alt="" className="pagefundingImage" style={{ width: '25rem' }} /></Link>
+          <Link href="https://www.ma-web.nl/"><Image src={mediacollege} alt="" className="pagefundingImage" style={{ width: '25rem', height: 'auto' }} /></Link>
+          <Link href="https://dezwijger.nl/"><Image src={PakhuisDeZwijger} alt="" className="pagefundingImage" style={{ width: '25rem' }} /></Link>
+        </div>
       </div>
     )
   },
@@ -375,69 +425,9 @@ export const extraPaths: SidePageDataProps[] = [
     component: "search",
     content: (
       <div className="pageLayout">
-        
+
       </div>
     )
-  },
-
-]
-
-export const pageInfo: InfoPageDataProps[] = [
-  {
-    pageTitle: "signup / login",
-    subtext: "GLUE is a community for designers, the general public, architects, brands, labels, showrooms, galleries, academies and other colleagues. This year the design route takes place from 19 – 22 September in 2024.Would you like to be part of the GLUE Community? A Membership is the way to go. During the year, all kind of events where you can meet the industry are organised. Monthly Community Cocktails and the opening party at Paradiso, to mention a few. You can become a member at any time. Want to participate in the GLUE design route of September in addition to becoming a member? Sign up as a participant and you are automatically a GLUE member too. Please subscribe before 1st of May 2024. It is possible to choose to be a GLUE member first, and decide later if you also want to also participate in the route. You can switch to a Participant Membership before 1st of May 2024. (You only pay the difference between a membership and the participation fee.)",
-  },
-  {
-    pageTitle: "about GLUE",
-    subtext: "A four days design-route for Amsterdam designers, the general public, architects, brands, labels, showrooms, galleries, academies and other colleagues. GLUE amsterdam connected by design connects all locations and provides a substantive and attractive program for colleagues and the public. Independence is essential, GLUE does not commit to one party, but all of them. Starting designers, knowledge institutes, renowned designers, and large agencies, diverse and ground floor suits Amsterdam, even though we sometimes have to climb a steep staircase. GLUE gives a profile to Amsterdam design and wants to connect the sector better and make it more discoverable. All participants have a dot on the map; we encourage them to organize something sensational and relevant during GLUE amsterdam connected by design.",
-  },
-
-  {
-    pageTitle: "Meet the Team",
-    subtext: "Welcome to GLUE amsterdam connected by design – where creativity, collaboration and a large variety of members converge to shape the future of the Amsterdam design scene. At the heart of our organisation are the visionary minds and dedicated individuals who form the backbone of GLUE. Allow us to introduce you to the driving forces behind our success:",
-  },
-  {
-    pageTitle: "GLUE participants 2023",
-    subtext: "Click on the link and see the map of Amsterdam with all participating brands, designers, studio's and academies of GLUE 2023",
-  },
-
-  {
-    pageTitle: "GLUE TV",
-    subtext: "The third edition of GLUE TV was recorded in the studio of Pakhuis de Zwijger, the platform for social innovation & creation in Amsterdam. GLUE TV focussed this year on members that were invited to explore the definition of design, and stretch its limits a bit; the so called STICKY members. But we used the platform also to interview GLUE’s Creative Citizens of Honour: Wouter Valkenier, Marian Duff and Janine Toussaint. Due to the storm Poly some interviews took place online. As part of the ‘Stretching the Definition of Design’ theme, Rubiah Balsem, Jeroen Junte, Gabrielle Kennedy, and Marsha Simon interviewed, amongst them: Georgie Frankel, Yamuna Forzani, Shahar Livne, Chen Yu Wang and Rink Schelling. Bas de Groot was in charge of editing. In the run-up to the design route in September, you can watch them via SALTO TV. Get inspired and check out all interviews on Youtube or SALTO TV.",
-  },
-  {
-    pageTitle: "Partners 2023",
-    subtext: "GLUE amsterdam connected by design is made possible by the support of our Founding and Cultural Partners. Their businesses are strongly tied with the culture and economy of Amsterdam. Who are they?",
-  },
-  {
-    pageTitle: "Press",
-    subtext: "Please contact Karin Dijksman for the most recent press release, high-res images and other press enquires.",
-  },
-  {
-    pageTitle: "Curated STICKY members",
-    subtext: "GLUE embraces the creative city even more warmly than before. Designers who contribute to broadening the field of design, making it more accessible, playful, and inspiring can be selected to become a curated STICKY member. Curated STICKY members receive the same package as normal STICKY members, but they participate free of charge. This group of members is selected by a panel of curators; it is nog possible to apply for a curated membership. The selection for 2023 was done by Rubiah Balsem, founder of Studio Balsem, and David Heldt, co-founder of GLUE. A new panel of curators will make a selection for GLUE 2024. By opening up the conversation about what design is, we also explore the boundaries between art, fashion, and design. Sometimes creatives don’t feel comfortable in one box and like to experiment with different concepts. And at the edge of the field is where it becomes interesting. It is a fragment of time that says something about the direction in which our society is moving, says Rubiah Balsem.",
-  },
-
-  {
-    pageTitle: "Creative citizens of honour",
-    subtext: "GLUE embraces the creative city even more warmly than before. Designers who contribute to broadening the field of design, making it more accessible, playful, and inspiring can be selected to become a curated STICKY member. Curated STICKY members receive the same package as normal STICKY members, but they participate free of charge. This group of members is selected by a panel of curators; it is nog possible to apply for a curated membership. The selection for 2023 was done by Rubiah Balsem, founder of Studio Balsem, and David Heldt, co-founder of GLUE. A new panel of curators will make a selection for GLUE 2024. ‘By opening up the conversation about what design is, we also explore the boundaries between art, fashion, and design. Sometimes creatives don’t feel comfortable in one box and like to experiment with different concepts. And at the edge of the field is where it becomes interesting. It is a fragment of time that says something about the direction in which our society is moving,’ says Rubiah Balsem. Three creative industry leaders have been chosen each year for their outstanding contribution to the city's social cohesion. Discover who they are!",
-  },
-  {
-    pageTitle: "mission statement",
-    subtext: "The power of local, connecting & inspiring GLUE’s primary mission is to connect the local design community, consisting of designers, creatives, architects, brands, showrooms, galleries, academies, and other colleagues by providing spaces to meet and moments to share and connect. Secondary mission is to connect the general public with the design community, which happens during the three days of the design-route. GLUE connects the members the whole year round, and opens the doors to the public each year in mid-September for a four days design route through Amsterdam. The three-day design route connects all GLUE locations and provides a substantive and attractive program for colleagues and the public. GLUE includes the whole scene; starting designers as well as renowned designers, small and big showrooms, hidden gems agencies and educational institutes. Smaller brands and individual creatives get exhibition space at a larger enterprise or hotel, these so called GLUE HUBS create new business relations for the members and make the program more challenging and diverse. GLUE aspires diversity and inclusivity, has an eye for sustainability and has a focus on wellbeing. GLUE gives profile to the Amsterdam design community, and wants to connect the sector better and makes it more discoverable. All members have a spot on the GLUE stage; we encourage them to organise something sensational and relevant during the four days GLUE amsterdam connected by design. GLUE organises community drinks in the months towards the design route at various different venues of the GLUE members. GLUE brings the sector together to meet and exchange ideas, and connect creativity, inclusivity and well-being to accomplish a future-proof and better-connected design sector for Amsterdam.",
-  },
-  {
-    pageTitle: "Glue Dallas",
-    subtext: "GLUE founders were inspired by Amsterdam’s vibrant activity to bring the creative sector together, now in its fourth edition the desire to expand the concept has reached the United States, with its Dallas edition set for April 3-5, 2024. GLUE dallas connected by design, will keep the essence of a three-day design-route that connects designers, architects, brands, showrooms, makers, artists, galleries, colleges and universities, and cultural organizations, with the aim of better connecting these creative fields. Dallas is the premiere market for this inaugural GLUE activation in the U.S. because of its historic civic history supporting design innovation. At GLUE dallas connected by design, designers will find opportunities to further build relevant connections, obtain contextual insight, and expand their network – all important reasons to launch in a design-forward city like Dallas. Being part of this dynamic expansion provides a unique opportunity to highlight and elevate the business of design throughout this vibrant and diverse city.",
-  },
-  {
-    pageTitle: "Glue Foundation",
-    subtext: "GLUE Foundation is set up to realise a cultural program for GLUE. The Foundation is responsible, among other programs, for the yearly nomination and ceremony celebrating the Creative Citizens of Honor in Paradiso, the curated STICKY members, and GLUE TV.",
-  },
-
-  {
-    pageTitle: "",
-    subtext: "",
   },
 
 ]
